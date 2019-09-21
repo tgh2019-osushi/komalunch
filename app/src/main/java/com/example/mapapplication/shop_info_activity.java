0ae994f18732330ab8fb4ff0b3dde201ee2bb2b6
yuri <yuri.pyu.flower@gmail.com>
2019-09-21 09:46:59 +0900

    マック情報画面追加

---
+++ app/src/main/java/com/example/mapapplication/shop_info_activity.java
@@ -0,0 +1,14 @@
+package com.example.mapapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shop_info_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_info_activity);
+    }
+}
